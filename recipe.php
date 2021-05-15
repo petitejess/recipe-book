220a2ee6721643e204ab05f2e9f5a82128a0bc2f
petitejess <jessica.gozali@outlook.com.au>
2021-05-15 22:26:12 +1000

    Add skeleton recipe page

---
+++ recipe.php
@@ -0,0 +1,55 @@
+<?php require 'head.php'; ?>
+
+<article>
+    <h1>Recipe Name</h1>
+
+    <figure>
+        <!-- <img> -->
+        <!-- <figcaption></figcaption> -->
+    </figure>
+
+    <div>Category | Duration | Potion</div>
+
+    <p>Short description about the recipe</p>
+
+    <h2>Ingredients</h2>
+    
+    <table>
+        <tr>
+            <td>qty</td>
+            <td>Item name</td>
+        </tr>
+        <tr>
+            <td>qty</td>
+            <td>Item name</td>
+        </tr>
+        <tr>
+            <td>qty</td>
+            <td>Item name</td>
+        </tr>
+        <tr>
+            <td>qty</td>
+            <td>Item name</td>
+        </tr>
+        <tr>
+            <td>qty</td>
+            <td>Item name</td>
+        </tr>
+        <tr>
+            <td>qty</td>
+            <td>Item name</td>
+        </tr>
+    </table>
+    
+    <h2>Directions</h2>
+
+    <ol>
+        <li>Step one of cooking.</li>
+        <li>Step two of cooking.</li>
+        <li>Step three of cooking.</li>
+        <li>Step four of cooking.</li>
+        <li>Step five of cooking.</li>
+    </ol>
+</article>
+
+<?php require 'footer.php'; ?>
