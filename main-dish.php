07b5b2e0376494f0627120e33a480a54d8977ae8
petitejess <jessica.gozali@outlook.com.au>
2021-05-15 22:33:28 +1000

    Add skeleton main dish page

---
+++ main-dish.php
@@ -0,0 +1,39 @@
+<?php require 'head.php'; ?>
+
+<h1>Main Dish Recipes</h1>
+
+<p>A collection of main dish recipes.</p>
+
+<div>
+    <figure>
+        <a href="#"><img src="#" alt="#"></a>
+    </figure>
+    <h3>Recipe Name</h3>
+    <div>Category | Duration | Potion</div>
+</div>
+
+<div>
+    <figure>
+        <a href="#"><img src="#" alt="#"></a>
+    </figure>
+    <h3>Recipe Name</h3>
+    <div>Category | Duration | Potion</div>
+</div>
+
+<div>
+    <figure>
+        <a href="#"><img src="#" alt="#"></a>
+    </figure>
+    <h3>Recipe Name</h3>
+    <div>Category | Duration | Potion</div>
+</div>
+
+<div>
+    <figure>
+        <a href="#"><img src="#" alt="#"></a>
+    </figure>
+    <h3>Recipe Name</h3>
+    <div>Category | Duration | Potion</div>
+</div>
+
+<?php require 'footer.php'; ?>
